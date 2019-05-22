ee84c0769ec55cfe35044a0fa6c185698274ecbe
fsocualaya <fernando.socualaya@utec.edu.pe>
2019-05-22 06:36:40 -0500

    Completed thinning

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,7 @@ int main(){
 	//Loads Image
 	Img image = loadImage("../resources/kimia99-images/donkey1.png");
 	int originalArea = image.rows*image.cols;
-	//double Scale;
-	//std::cin>>Scale;	
-	
-	//cv::resize(image, image, cv::Size(),Scale,Scale);
-	
+
 	//Get binary Matrix based on image
 	intMatrix imgMat = getBinMat(image);
 	if(POTENTIAL_FIELDS){
@@ -39,8 +35,8 @@ int main(){
 
 	cv::waitKey(0);
 
-//	plotDoubleMatDots(xForcesMatrix);
-//	plotDoubleMatDots(yForcesMatrix);
+	//	plotDoubleMatDots(xForcesMatrix);
+	//	plotDoubleMatDots(yForcesMatrix);
 
 
 	//cv::imshow("original", image);
@@ -50,19 +46,8 @@ int main(){
 	//cv::waitKey(0);
 	}
 	else{	
-		/*intMatrix invertedMatrix = invertColors(imgMat);
-		Img thinned = thinning(invertedMatrix);
-		cv::imshow("thinned", thinned);
-		cv::imshow("original", image);
-		cv::waitKey(0);*/
 		Img thinned, auxImage;
-		/*double duration = thinningExecutionTime(image, thinned);
-		cv::imshow("Original", image);
-		cv::imshow("Thinned", thinned);
-		std::fstream executionTimes("../results/executionTime.csv", std::ios::app);	
-		executionTimes<<originalArea*pow(Scale,2)<<','<<duration<<'\n';
-		cv::waitKey(0);*/
-		std::fstream executionTimes("../results/thinningExecutionTime.csv", std::ios::out);
+		std::fstream executionTimes("../results/thinning/runtime/thinningExecutionTime.csv", std::ios::out);
 		executionTimes<<"imageArea,executionTime\n";
 		for(double Scale=0.5;Scale<=2.0;Scale+=0.25){
 			std::cout<<Scale<<'\n';
@@ -73,8 +58,7 @@ int main(){
 			}
 		}
 		executionTimes.close();
-		/*cv::resize(image,image,cv::Size(),100/100.0,100/100.0);
-		double duration = thinningExecutionTime(image, thinned);*/
+		writeImage(thinned, "../results/thinning/imgs/skeleton_donkey1.png");
 		cv::imshow("original", auxImage);
 		cv::imshow("Thinned", thinned);
 		cv::waitKey(0);
